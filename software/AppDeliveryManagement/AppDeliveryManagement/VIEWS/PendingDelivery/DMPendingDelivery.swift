4f8a04fb29876f4ea83cc80142b36daa84d8fd00
maarkeez <d.marquez.delgado.91@gmail.com>
2018-03-18 18:25:07 +0100

    [AppDeliveryManagement] - Created proyecto.

---
+++ software/AppDeliveryManagement/AppDeliveryManagement/VIEWS/PendingDelivery/DMPendingDelivery.swift
@@ -0,0 +1,35 @@
+//
+//  DMPendingDelivery.swift
+//  AppDeliveryManagement
+//
+//  Created by David Márquez Delgado on 18/3/18.
+//  Copyright © 2018 David Márquez Delgado. All rights reserved.
+//
+
+import UIKit
+
+class DMPendingDelivery: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ software/AppDeliveryManagement/AppDeliveryManagement/VIEWS/PendingDelivery/DMPendingDeliveryCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DMPendingDeliveryCellTableViewCell.swift
+//  AppDeliveryManagement
+//
+//  Created by David Márquez Delgado on 18/3/18.
+//  Copyright © 2018 David Márquez Delgado. All rights reserved.
+//
+
+import UIKit
+
+class DMPendingDeliveryCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
