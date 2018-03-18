c96f9296d37fa88fa871293814a8c4671d15e1f1
maarkeez <d.marquez.delgado.91@gmail.com>
2018-03-18 18:35:30 +0100

    [1.0.0] - First view and classes

---
+++ software/AppDeliveryManagement/AppDeliveryManagement/ENTITIES/DMPedido.swift
@@ -0,0 +1,9 @@
+//
+//  DMPedido.swift
+//  AppDeliveryManagement
+//
+//  Created by David Márquez Delgado on 18/3/18.
+//  Copyright © 2018 David Márquez Delgado. All rights reserved.
+//
+
+import Foundation
--- software/AppDeliveryManagement/AppDeliveryManagement/VIEWS/PendingDelivery/DMPendingDelivery.swift
+++ software/AppDeliveryManagement/AppDeliveryManagement/VIEWS/PendingDelivery/DMPendingDelivery.swift
@@ -7,29 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
 class DMPendingDelivery: UIViewController {
-
+    
+    //MARK: - Variables locales
+    
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var myMap: MKMapView!
+    @IBOutlet weak var myTable: UITableView!
+    
+    //MARK: - IBActions
+    
+    //MARK: - LIFE VC
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        myTable.delegate = self
+        myTable.dataSource = self
     }
     
+    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//MARK: - Extensions
+extension DMPendingDelivery : UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
-    */
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
 }
